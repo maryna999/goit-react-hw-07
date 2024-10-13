c61c774f4d2edc776d803635483bcf4c76e28275
mary <mkovalova29@gmail.com>
2024-10-13 16:19:49 +0300

    add HW7

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,21 +2,15 @@ import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteContact } from "../../redux/contactsOps";
-import { selectContacts } from "../../redux/contactsSlice";
-import { selectNameFilter } from "../../redux/filtersSlice";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 const ContactList = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={s.list}>
-      {filteredContacts.length === 0 && filter && <li>No matches found!</li>}
+      {filteredContacts.length === 0 && <li>No matches found!</li>}
       {filteredContacts.map((contact) => (
         <Contact
           key={contact.id}
